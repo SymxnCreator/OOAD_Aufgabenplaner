0252221ca58dfafd9deb7008d1fd75e0f64f0ac3
mediekma <megan.diekmann@hs-osnabrueck.de>
2022-08-03 22:50:24 +0200

    Statistic.java weiter bearbeitet

--- src/sample/Models/Statistic.java
+++ src/sample/Models/Statistic.java
@@ -28,11 +28,34 @@ public class Statistic {
         return counter;
     }
 
-    static int getSumTasksDone(TaskList taskList) {
+    static int getSumTasksDone(TaskList taskList) { //in TaskLists rein????
         int counter = 0;
         for (Task task : taskList.getTasks()) {
             if (task.isFinished()) counter++;
         }
         return counter;
+    }
+
+    static TaskList getMostSuccessfulGroup(ArrayList<TaskList> taskLists) {
+        if (taskLists.size() == 0) return null;
+
+        TaskList tl1 = taskLists.get(0);
+        double tl1TasksDoneInPercent = getSumTasksDoneInPercent(tl1);
+
+        for (int i = 1; i < taskLists.size(); i++) {
+            TaskList tl2 = taskLists.get(i);
+            double tl2TasksDoneInPercent = getSumTasksDoneInPercent(tl2);
+
+            if (tl1TasksDoneInPercent < tl2TasksDoneInPercent) {
+                tl1 = tl2;
+                tl1TasksDoneInPercent = tl2TasksDoneInPercent;
+            }
+        }
+
+        return tl1;
+    }
+
+    static double getSumTasksDoneInPercent(TaskList taskList) {
+        return (getSumTasksDone(taskList) * 100.0) / taskList.getTasks().size();
     }
 }
