0b8a3997eea328fcb2f9668934fef72c4f6cd153
Dressel107 <88779152+Dressel107@users.noreply.github.com>
2022-08-06 21:38:14 +0200

    Btn und Icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ public class Main extends Application
     public void start(Stage primaryStage) throws Exception
     {
         Parent root = FXMLLoader.load(getClass().getResource("Views/MainView.fxml"));
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("Views/Resources/calendar.png")));
         primaryStage.setTitle("Aufgabenplaner");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
